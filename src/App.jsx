4ccd4ab20a23fdd907b8ef0348d644187ed0e39c
sku11ster <murtazaahmed022@gmail.com>
2023-11-08 21:48:26 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Hero,PopularProducts,SuperQuality,Services,CustomerReviews,Subscribe,Footer,
+  SpecialOffers,} from './sections';
+
+
+const App = () => (
+  <main className='relative'>
+    Nav
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <Hero/>
+    </section>
+
+
+
+  </main>
+
+);
+
+export default App;
